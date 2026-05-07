0a36d8b324f0d224e7cccef558c68adce38620a2
v <cryi@tutanota.com>
2026-05-07 06:28:19 +0200

    updated deps

--- _app/immutable/entry/start.BV-hcNeh.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Bai8xt5v.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/0.B-YYTMjo.js
@@ -0,0 +1,1 @@
+import{b as e,_ as n}from"../chunks/BNh3c4xZ.js";export{e as component,n as universal};
